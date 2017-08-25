4658e901f67280858e1315fbcbd91aee6e3d85fc
Prandtl <vasiliy.uchanev@prandtl.co>
2017-08-25 14:56:05 +0500

    removed Windows part of example

--- ValueConversion/ValueConversion.WindowsStore/Bootstrap/ColorPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace ValueConversion.UI.WindowsStore.Bootstrap
-{
-    public class ColorPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Color.PluginLoader>
-    {
-    }
-}
--- ValueConversion/ValueConversion.WindowsStore/Bootstrap/VisibilityPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace ValueConversion.UI.WindowsStore.Bootstrap
-{
-    public class VisibilityPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Visibility.PluginLoader>
-    {
-    }
-}
